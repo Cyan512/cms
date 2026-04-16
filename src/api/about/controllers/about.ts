1866788300d82868d03330409d5e1b3517692b14
Cyan512 <182935@unsaac.edu.pe>
2026-04-16 08:46:35 -0500

    about api

--- src/api/about/controllers/about.ts
+++ src/api/about/controllers/about.ts
@@ -11,10 +11,39 @@ export default factories.createCoreController('api::about.about', ({ strapi }) =
             populate: {
                 content: {
                     on: {
-
+                        'shared.section-hero': {
+                            populate: {
+                                header: true,
+                                image: {
+                                    populate: {
+                                        src: true
+                                    }
+                                },
+                            }
+                        },
+                        'about.about-story': {
+                            populate: {
+                                header: true,
+                                image: {
+                                    populate: {
+                                        src: true
+                                    }
+                                },
+                                stats: true,
+                                values: true,
+                            }
+                        },
+                        'shared.section-cta': {
+                            populate: {
+                                header: true,
+                                link: true
+                            }
+                        }
                     }
                 }
             }
-        }
+        };
+        const { data, meta } = await super.find(ctx);
+        return { data, meta };
     }
 }));
