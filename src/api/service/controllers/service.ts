f5007dbb213de72dbec15a18794f43b4282f3552
Cyan512 <182935@unsaac.edu.pe>
2026-04-21 19:55:24 -0500

    services

--- src/api/service/controllers/service.ts
+++ src/api/service/controllers/service.ts
@@ -4,4 +4,20 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::service.service');
+export default factories.createCoreController('api::service.service', ({ strapi }) => ({
+  async find(ctx) {
+    ctx.query = {
+      ...ctx.query,
+      populate: {
+        images: {
+          populate: {
+            src: true,
+          },
+        },
+      },
+    };
+
+    const { data, meta } = await super.find(ctx);
+    return { data, meta };
+  },
+}));
